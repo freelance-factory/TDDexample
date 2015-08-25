ebbeb968e69e4462c4338bca399fc643923739ac
Leonardo Riviere <leonardoriviere@MacBook-Pro-de-Leonardo.local>
2015-08-25 02:03:19 -0300

    Changes made to the tests. They all work now. Even the one with NullPointerException.

--- src/main/java/com/tdd/example/helper/StringHelper.java
+++ src/main/java/com/tdd/example/helper/StringHelper.java
@@ -3,11 +3,10 @@ package com.tdd.example.helper;
 public class StringHelper {
 
     public char[] createCharArray(String a) {
-        char[] charArray = new char[0];
+        char[] charArray = new char[a.length()];
         for (int i = 0; i < a.length(); i++) {
             charArray[i] = a.charAt(i);
         }
         return charArray;
     }
-
 }
